af3a87676e6d744b8b6bcfffd5e944391ed6e00b
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2026-08-29 00:59:25 +1000

    Big moves

--- main.py
+++ main.py
@@ -1,118 +1,55 @@
-import json
-from pathlib import Path
+from nicegui import ui
 
-import platformdirs
-import tomlkit as toml
+if __name__ in {"__main__", "__mp_main__"}:
+    with ui.dialog().props("persistent") as dialog, ui.card():
+        ui.label("Hello world!")
+        ui.button("Close", on_click=dialog.close)
 
-game_save = Path(platformdirs.user_data_dir() + "Low") / "Mipumi Games" / "BlackJacket"
+    dialog.open()
 
+    ui.notify(
+        "Unknown cards detected! Please declare them in the Data Collector.",
+        type="warning",
+    )
 
-def flatten_dict(data: dict) -> list:
-    # Designed for use within this script
-    result = []
+    with ui.tabs().classes("w-full") as tabs:
+        collector = ui.tab("Data Collector")
+        viewer = ui.tab("Deck Viewer")
+        db = ui.tab("Known Cards")
 
-    for group in data:
-        for suit in data[group]:
-            for key in data[group][suit]:
-                result.append(key)
-                # result[key] = d[group][suit]
+    with ui.tab_panels(tabs, value=collector).classes("w-full"):
+        with ui.tab_panel(collector):
+            ui.linear_progress(0.5)
+            ui.label("What card is this?")
+            ui.radio(["Blank", "Awakened", "Face Card"]).props("inline")
+            with ui.row():
+                ui.input(
+                    label="Suit",
+                    placeholder="diamonds",
+                )
+                with ui.button(icon="info"):
+                    ui.tooltip("Type all suit names in lower case with plurals.")
+            ui.input(
+                label="Value",
+                placeholder="ace",
+            )
+            ui.button(
+                "Continue",
+                on_click=lambda: (
+                    viewer.disable() if viewer.enabled else viewer.enable()
+                ),
+            )
+        with ui.tab_panel(viewer):
+            ui.label("Second tab")
 
-    return result
+            with ui.image(
+                "https://blackjacket.wiki.gg/images/Card_Diamonds_2.png"
+            ).classes("w-16"):
+                with ui.context_menu():
+                    ui.menu_item("Set VFX")
+                    ui.menu_item("Replace")
+                    ui.menu_item("Remove")
+                    # ui.separator()
+                    # ui.menu_item("Reset", auto_close=False)
 
-
-def find_key(to_find: str, data: dict) -> tuple[str, str, str]:
-    group = ""
-    suit_or_set = ""
-    value = ""
-
-    for data_group in data:
-        for data_suit in data[data_group]:
-            for key in data[data_group][data_suit]:
-                if key == to_find:
-                    group = data_group
-                    suit_or_set = data_suit
-                    value = data[data_group][data_suit][key]
-                    return (group, suit_or_set, value)
-
-    return (group, suit_or_set, value)
-
-
-if __name__ == "__main__":
-    with open(game_save / "campaignSave.json") as f:
-        save = json.load(f)
-
-    with open("database.toml") as f:
-        mapped_guids = toml.load(f)
-
-    known_guids = flatten_dict(mapped_guids)
-
-    print("*** Data Collection ***")
-    for current in range(len(save["DeckSave"]["GameCardSaves"])):
-        guid = save["DeckSave"]["GameCardSaves"][current]["GameCardGuid"]
-
-        if guid not in known_guids:
-            print(f"{guid} does not exist. Marking it as bloody.")
-
-            save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Bloody"] = True
-            save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Sliced"] = True
-            hollow = save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Ghostly"]
-            if hollow:
-                save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Ghostly"] = False  # fmt: skip
-
-            with open(game_save / "campaignSave.json", "w") as f:
-                json.dump(save, f, indent=4)
-
-            try:
-                print("What card is this?")
-                group = input("Type (blank/awakened/face): ")
-                if group == "face":
-                    suit_or_set = input("Set: ")
-                else:
-                    suit_or_set = input("Suit: ")
-                # We're not doing comparisons, so casefold is unnecessarily aggressive
-                suit_or_set = suit_or_set.lower()
-                value = input("Value: ")
-
-                if suit_or_set not in mapped_guids[group]:
-                    mapped_guids[group][suit_or_set] = {}
-
-                mapped_guids[group][suit_or_set][guid] = value
-                known_guids = flatten_dict(mapped_guids)
-
-                with open("database.toml", "w") as f:
-                    toml.dump(mapped_guids, f)
-
-            finally:
-                # fmt: off
-                save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Bloody"] = False
-                save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Sliced"] = False
-                if hollow:
-                    save["DeckSave"]["GameCardSaves"][current]["VfxOptions"]["Ghostly"] = True
-                # fmt: on
-
-                with open(game_save / "campaignSave.json", "w") as f:
-                    json.dump(save, f, indent=4)
-
-    print("*** Deck Analysis ***")
-    for current in range(len(save["DeckSave"]["GameCardSaves"])):
-        guid = save["DeckSave"]["GameCardSaves"][current]["GameCardGuid"]
-        group, suit_or_set, value = find_key(guid, mapped_guids)
-
-        match group:
-            case "awakened":
-                try:
-                    value = int(value)
-                    print(f"Awakened {value} of {suit_or_set.title()}")
-                except ValueError:
-                    if value == "Ace":
-                        print(f"Awakened {value} of {suit_or_set.title()}")
-                    else:
-                        print(value)
-
-            case "blank":
-                if suit_or_set == "death":
-                    print(f"{value} of {suit_or_set.title()}")
-                else:
-                    print(f"Blank {value} of {suit_or_set.title()}")
-            case "face":
-                print(value)
+    ui.run(dark=None, native=True, reload=False)
